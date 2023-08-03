8036ac565899eeef5a5ac712c98e01620cee0d46
numanberk <141316264+numanberk@users.noreply.github.com>
2023-08-03 11:54:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const virtualGift = document.getElementById("virtualGift");
+
+    virtualGift.addEventListener("click", function () {
+        // Add your code here to reveal the surprise or play a message
+        alert("Yüz yüze kutlayamadığımız için gerçekten çok üzgünüz ama yaz tatilinde doğan biri olarak buna alışmışsındır diye umuyoruz ve nacizane hediyemizi takdim ediyoruz güle güle kullan 🙌 RA-TJVHK6S7DYH9KRSC -Osman ve Numan");
+    });
+});
